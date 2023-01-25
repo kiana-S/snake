079294e2df3a8cb3f72931dd920cda7037bdfdba
Kiana Sheibani <kiana.a.sheibani@gmail.com>
2023-01-25 15:49:42 -0500

    Fix orientation error

--- src/Game/State.hs
+++ src/Game/State.hs
@@ -3,8 +3,8 @@ module Game.State where
 data Direction = U | D | L | R
 
 movePos :: Direction -> (Int, Int) -> (Int, Int)
-movePos U (x, y) = (x, y - 1)
-movePos D (x, y) = (x, y + 1)
+movePos U (x, y) = (x, y + 1)
+movePos D (x, y) = (x, y - 1)
 movePos L (x, y) = (x - 1, y)
 movePos R (x, y) = (x + 1, y)
 
